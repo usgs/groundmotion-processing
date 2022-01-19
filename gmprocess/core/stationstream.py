Fix inconsistent use of units vs units_type

gmprocess/core/stationstream.py
@@ -416,8 +416,8 @@ class StationStream(Stream):
 
 
 def _channel_from_stats(stats):
-    if stats.standard.units in UNITS:
-        units = UNITS[stats.standard.units]
+    if stats.standard.units_type in UNITS:
+        units = UNITS[stats.standard.units_type]
     else:
         units = ""
     instrument = stats.standard.instrument
@@ -444,7 +444,7 @@ def _channel_from_stats(stats):
     else:
         # we may have instrument sensitivity...
         frequency = 1 / stats["standard"]["instrument_period"]
-        units = stats.standard.units
+        units = stats.standard.units_type
         if not np.isnan(stats["standard"]["instrument_sensitivity"]):
             sens = stats["standard"]["instrument_sensitivity"]
         else:

gmprocess/metrics/reduction/arias.py
@@ -73,7 +73,7 @@ class Arias(Reduction):
             # Create a copy of stats so we don't modify original data
             stats = trace.stats.copy()
             channel = stats.channel
-            stats.standard.units = "vel"
+            stats.standard.units_type = "vel"
             stats.npts = len(arias_intensity)
             arias_stream.append(StationTrace(arias_intensity, stats))
             arias_intensities[channel] = np.abs(np.max(arias_intensity))

tests/gmprocess/metrics/imt/arias_test.py
@@ -34,7 +34,6 @@ def test_arias():
         "delta": delta,
         "sampling_rate": sr,
         "npts": len(acc),
-        "units": "m/s/s",
         "channel": "HN1",
         "standard": {
             "corner_frequency": np.nan,
@@ -51,7 +50,7 @@ def test_arias():
             "process_time": "",
             "horizontal_orientation": np.nan,
             "vertical_orientation": np.nan,
-            "units": "acc",
+            "units": "m/s/s",
             "units_type": "acc",
             "instrument_sensitivity": np.nan,
             "instrument_damping": np.nan,

tests/gmprocess/metrics/imt/duration_test.py
@@ -35,7 +35,6 @@ def test_duration():
         "delta": delta,
         "sampling_rate": sr,
         "npts": len(acc),
-        "units": "m/s/s",
         "channel": "HN1",
         "standard": {
             "corner_frequency": np.nan,
@@ -52,7 +51,7 @@ def test_duration():
             "process_time": "",
             "horizontal_orientation": np.nan,
             "vertical_orientation": np.nan,
-            "units": "acc",
+            "units": "m/s/s",
             "units_type": "acc",
             "instrument_sensitivity": np.nan,
             "instrument_damping": np.nan,