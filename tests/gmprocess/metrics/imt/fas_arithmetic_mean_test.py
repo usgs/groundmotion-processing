Fix units bug for broadband instruments

tests/gmprocess/metrics/imt/fas_arithmetic_mean_test.py
@@ -48,7 +48,7 @@ def test_fas():
         trace = StationTrace(
             data=accels,
             header={
-                "channel": "H" + str(idx),
+                "channel": "HN" + str(idx),
                 "delta": dt,
                 "standard": {
                     "corner_frequency": np.nan,