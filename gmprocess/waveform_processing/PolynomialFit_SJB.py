195bd78fc7f989824607bf0393e8ee983f40558d
gparker-usgs <gparker@usgs.gov>
2022-01-26 16:02:46 -0800

    update SNR fchp check after rebasing

--- gmprocess/waveform_processing/PolynomialFit_SJB.py
+++ gmprocess/waveform_processing/PolynomialFit_SJB.py
@@ -65,13 +65,18 @@ def PolynomialFit_SJB(
             )
 
             if out[0] == True:
-                initial_corners["highpass"] = out[1]
-                tr.setParameter("corner_frequencies", initial_corners)
-                logging.debug(
-                    "Ridder fchp passed to trace stats = %s with misfit %s",
-                    out[1],
-                    out[2],
-                )
+                if out <= initial_corners["highpass"]:
+                    logging.debug(
+                        "Polyfit returns value less than SNR fchp. Adopting SNR fchp"
+                    )
+                else:
+                    initial_corners["highpass"] = out[1]
+                    tr.setParameter("corner_frequencies", initial_corners)
+                    logging.debug(
+                        "Ridder fchp passed to trace stats = %s with misfit %s",
+                        out[1],
+                        out[2],
+                    )
 
             else:
                 tr.fail(
