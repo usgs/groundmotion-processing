742c586716adb2fddaa1f568551a8adab5e9d0f1
Gabe Ferragut <gferragut@usgs.gov>
2022-06-14 10:26:59 -0600

    Reformat event and station map popup information, rebuild docs with updated map

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.11.dev467+ge16ffeea.d20220610',
+    VERSION: '1.1.11.dev468+g45643b49.d20220614',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
