8475fea75b660798231f5a5b72a708f5298ec187
Eric Thompson <emthompson@usgs.gov>
2022-09-29 09:04:28 -0600

    Revert "DOCS: Add tutorial with example of high-level scripting of GMrecordsApp (#1004)"
    This reverts commit 47c4bc62e0d4c749e9708126029aae005bf1eb5e.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev15+g775d9ee6.d20220928',
+    VERSION: '1.2.1.dev8+g1cb0b333.d20220918',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
