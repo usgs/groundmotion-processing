cddafe8029c116ac3a885f3d2242750728b44d64
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 14:34:55 +0000

    Adding tarfile member sanitization to extractall()

--- src/gmprocess/subcommands/import_data.py
+++ src/gmprocess/subcommands/import_data.py
@@ -78,7 +78,26 @@ class ImportModule(base.SubcommandModule):
                 elif tarfile.is_tarfile(dst):
                     logging.info(f"Extracting {dst}")
                     with tarfile.open(dst, "r") as tar_file:
-                        tar_file.extractall(raw_dir)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner) 
+                            
+                        
+                        safe_extract(tar_file, raw_dir)
                     os.remove(dst)
             elif os.path.isdir(import_path):
                 src = import_path
