831634ff910adcbab45ebc45ea1f82a398a87169
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:29:40 +0000

    Adding numeric_owner as keyword arguement

--- src/gmprocess/subcommands/import_data.py
+++ src/gmprocess/subcommands/import_data.py
@@ -94,7 +94,7 @@ class ImportModule(base.SubcommandModule):
                                 if not is_within_directory(path, member_path):
                                     raise Exception("Attempted Path Traversal in Tar File")
                         
-                            tar.extractall(path, members, numeric_owner) 
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
                             
                         
                         safe_extract(tar_file, raw_dir)
