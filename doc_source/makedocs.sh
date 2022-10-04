e62e1d50b027653785c5f21ac189d8bf5be256d9
emthompson@usgs.gov <emthompson@usgs.gov>
2022-10-04 15:49:51 -0600

    Version 1.2.1

--- doc_source/makedocs.sh
+++ doc_source/makedocs.sh
@@ -4,6 +4,10 @@
 # You can run `rm -r _build` to force a complete rebuild of the documentation.
 
 make html
+if [ $? -ne 0 ];then
+    echo "Failed make HTML. Exiting."
+    exit 1
+fi
 rm -fr ../docs
 cp -r _build/html ../docs
 touch ../docs/.nojekyll
--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev20+g8f162f73',
+    VERSION: '1.2.1.dev20+g4c710777.d20221004',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
