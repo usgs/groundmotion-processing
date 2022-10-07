96c1c829a97b2ab4d19b9112edf23d5bf6d18ea6
Brad Aagaard <baagaard@usgs.gov>
2022-10-06 21:51:42 -0600

    FIX: Update projects unit tests. Add additional tests.

--- tests/gmprocess/subcommands/projects_test.py
+++ tests/gmprocess/subcommands/projects_test.py
@@ -10,7 +10,7 @@ from gmprocess.utils import constants
 def test_projects(script_runner):
     try:
         # Need to create profile first.
-        setup_inputs = io.StringIO("2\ntest\n\n\nname\ntest@email.com\n")
+        setup_inputs = io.StringIO("test1\n\n\nname\ntest@email.com\n")
         ret = script_runner.run("gmrecords", "projects", "-c", stdin=setup_inputs)
         setup_inputs.close()
         assert ret.success
@@ -23,23 +23,33 @@ def test_projects(script_runner):
         setup_inputs.close()
         assert ret.success
 
+        setup_inputs = io.StringIO("test1\n")
+        ret = script_runner.run("gmrecords", "projects", "-c", stdin=setup_inputs)
+        setup_inputs.close()
+        assert ret.success
+        assert "Project 'test1' already exists." in ret.stderr
+
         ret = script_runner.run("gmrecords", "projects", "-l")
         assert ret.success
+        assert "Project: test1" in ret.stdout
         assert "Project: test2 **Current Project**" in ret.stdout
 
-        ret = script_runner.run("gmrecords", "projects", "-s", "test")
+        ret = script_runner.run("gmrecords", "projects", "-s", "test1")
         assert ret.success
+        assert "Project: test1 **Current Project**" in ret.stdout
 
-        ret = script_runner.run("gmrecords", "projects", "--rename", "test", "tested")
+        ret = script_runner.run("gmrecords", "projects", "--rename", "test2", "tested")
         assert ret.success
+        assert "Renamed 'test2' to 'tested'." in ret.stdout
 
         setup_inputs = io.StringIO("y\n")
         ret = script_runner.run(
-            "gmrecords", "projects", "-d", "test2", stdin=setup_inputs
+            "gmrecords", "projects", "-d", "tested", stdin=setup_inputs
         )
         setup_inputs.close()
         assert ret.success
-        assert "Project: test2" not in ret.stdout
+        assert "Project: tested" not in ret.stdout
+        assert "Project: test1 **Current Project**" in ret.stdout
 
     except Exception as ex:
         raise ex
